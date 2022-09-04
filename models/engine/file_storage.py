997267f3b8951f703dffe53841278226d6fcc2ab
kezzyj <karachidavid31@gmail.com>
2022-09-04 13:03:14 -0700

    working hardly

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,61 +1,245 @@
+
 #!/usr/bin/python3
+
 """
-This module containsa class FileStorage
+
+Handles I/O, writing and reading, of JSON for storage of all class instances
+
 """
 
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.state import State
-from models.review import Review
 
-classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'City': City, 'Amenity': Amenity, 'State': State,
-               'Review': Review}
+from models import base_model, amenity, city, place, review, state, user
+
+from datetime import datetime
+
+
+
+strptime = datetime.strptime
+
+to_json = base_model.BaseModel.to_json
+
+
+
+
 
 class FileStorage:
+
     """
-    Implementation of the class
+
+        handles long term storage of all class instances
+
     """
-    __file_path = "file.json"
+
+    CNC = {
+
+        'BaseModel': base_model.BaseModel,
+
+        'Amenity': amenity.Amenity,
+
+        'City': city.City,
+
+        'Place': place.Place,
+
+        'Review': review.Review,
+
+        'State': state.State,
+
+        'User': user.User
+
+    }
+
+    """CNC - this variable is a dictionary with:
+
+    keys: Class Names
+
+    values: Class type (used for instantiation)
+
+    """
+
+    __file_path = './dev/file.json'
+
     __objects = {}
 
-    def all(self):
+
+
+    def all(self, cls=None):
+
         """
-        Returns the dictionary __objects
+
+            returns private attribute: __objects
+
         """
-        return self.__objects
+
+        if cls is not None:
+
+            new_objs = {}
+
+            for clsid, obj in FileStorage.__objects.items():
+
+                if type(obj).__name__ == cls:
+
+                    new_objs[clsid] = obj
+
+            return new_objs
+
+        else:
+
+            return FileStorage.__objects
+
+
 
     def new(self, obj):
+
         """
-        Sets in __objects the obj with key <obj class name>.id
+
+            sets / updates in __objects the obj with key <obj class name>.id
+
         """
-        if obj:
-            key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+
+        bm_id = "{}.{}".format(type(obj).__name__, obj.id)
+
+        FileStorage.__objects[bm_id] = obj
+
+
 
     def save(self):
+
         """
-        Serializes __objects to the JSON file (path: __file_path)
+
+            serializes __objects to the JSON file (path: __file_path)
+
         """
-        json_obj = {}
-        for key in self.__objects:
-            json_obj[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
-            json.dump(json_obj, f)
+
+        fname = FileStorage.__file_path
+
+        storage_d = {}
+
+        for bm_id, bm_obj in FileStorage.__objects.items():
+
+            storage_d[bm_id] = bm_obj.to_json(saving_file_storage=True)
+
+        with open(fname, mode='w', encoding='utf-8') as f_io:
+
+            json.dump(storage_d, f_io)
+
+
 
     def reload(self):
+
         """
-        Deserializes the JSON file to __objects (only if the JSON
-        file (__file_path) exists
+
+            if file exists, deserializes JSON file to __objects, else nothing
+
         """
+
+        fname = FileStorage.__file_path
+
+        FileStorage.__objects = {}
+
         try:
-            with open(self.__file_path, 'r') as f:
-                j_obj = json.load(f)
-            for k in j_obj:
-                self.__objects[k] = classes[j_obj[k]["__class__"]](**j_obj[k])
-        except Exception:
+
+            with open(fname, mode='r', encoding='utf-8') as f_io:
+
+                new_objs = json.load(f_io)
+
+        except:
+
+            return
+
+        for o_id, d in new_objs.items():
+
+            k_cls = d['__class__']
+
+            FileStorage.__objects[o_id] = FileStorage.CNC[k_cls](**d)
+
+
+
+    def delete(self, obj=None):
+
+        """
+
+            deletes obj from __objects if it's inside
+
+        """
+
+        if obj:
+
+            obj_ref = "{}.{}".format(type(obj).__name__, obj.id)
+
+            all_class_objs = self.all(obj.__class__.__name__)
+
+            if all_class_objs.get(obj_ref):
+
+                del FileStorage.__objects[obj_ref]
+
+            self.save()
+
+
+
+    def delete_all(self):
+
+        """
+
+            deletes all stored objects, for testing purposes
+
+        """
+
+        try:
+
+            with open(FileStorage.__file_path, mode='w') as f_io:
+
+                pass
+
+        except:
+
             pass
+
+        del FileStorage.__objects
+
+        FileStorage.__objects = {}
+
+        self.save()
+
+
+
+    def close(self):
+
+        """
+
+            calls the reload() method for deserialization from JSON to objects
+
+        """
+
+        self.reload()
+
+
+
+    def get(self, cls, id):
+
+        """
+
+            retrieves one object based on class name and id
+
+        """
+
+        if cls and id:
+
+            fetch_obj = "{}.{}".format(cls, id)
+
+            all_obj = self.all(cls)
+
+            return all_obj.get(fetch_obj)
+
+        return None
+
+
+
+    def count(self, cls=None):
+
+        """
+
+        count of all objects in storage
+
+        """
+
